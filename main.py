76bf9a9568cfac1bf590cf8a59a6f49f98bd9512
Devin PierreEmmanuel <pierre-emmanuel.devin2@etu.univ-lorraine.fr>
2023-04-27 18:17:14 +0200

    added return from function and assignments after let

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3.11
+from abc import abstractmethod
 import copy
 import json
 import os.path
 import subprocess
 import time
-from typing import List, Optional, Tuple, Dict
+from typing import List, Tuple, Dict, overload
 import sys
+from typing_extensions import override
 
 STATIC_MEMORY_SIZE = 1024 * 1024 * 8
 MAX_ARGS = 5
@@ -16,6 +18,7 @@ string_length = 0
 debug = False
 if 'DEBUG' in os.environ:
     debug = True
+
 
 info_cmd = True
 if 'NO_CMD_INFO' in os.environ:
@@ -76,6 +79,7 @@ class TokenType(int):
     EQ = auto()
     WHILE = auto()
     IF = auto()
+    RETURN = auto()
     LET = auto()
     CONST = auto()
     LEFT_SHIFT = auto()
@@ -153,7 +157,7 @@ class Lexer:
                 self.cursor_on_line = 1
 
     def __next__(self):
-        assert TokenType.TokenType_NUMBERS == 27
+        assert TokenType.TokenType_NUMBERS == 28
         if self.cursor >= len(self.program_string):
             raise StopIteration
         self.trim_left()
@@ -190,6 +194,8 @@ class Lexer:
                 type_ = TokenType.IF
             elif literal == 'while':
                 type_ = TokenType.WHILE
+            elif literal == 'return':
+                type_ = TokenType.RETURN
             elif literal == 'fun':
                 type_ = TokenType.FUNCTION
             elif literal == 'let':
@@ -301,9 +307,9 @@ class AST():
     def __repr__(self):
         return self.__str__()
 
+    @abstractmethod
     def generate(self, generator, out_file):
-        assert TokenType.TokenType_NUMBERS == 21
-
+        pass
 
 class BinaryOperator(AST):
     def __init__(self, token: Token, left: AST, right: AST):
@@ -327,7 +333,7 @@ class BinaryOperator(AST):
         )
 
     def generate(self, generator, out_file):
-        assert TokenType.TokenType_NUMBERS == 27
+        assert TokenType.TokenType_NUMBERS == 28
         if self.token.type == TokenType.LEFT_SHIFT:
             self.left.generate(generator, out_file)
             print(f"push rax", file=out_file)
@@ -529,6 +535,7 @@ class FunctionCall(AST):
         print(f"call {generator.functions[identifier]}", file=out_file)
 
 
+
 class BlockNode(AST):
     def __init__(self, token: Token, statements: List[AST]):
         super().__init__(token)
@@ -573,6 +580,18 @@ class AssignNode(AST):
             print(f"mov qword {generator.variables[identifier]}, rax", file=out_file)
 
 
+class ReturnNode(AST):
+    def __init__(self, token: Token, expression: AST):
+        super().__init__(token)
+        self.expression = expression
+
+    def to_dict(self):
+        return dict(expression=self.expression.to_dict())
+
+    def generate(self, generator, out_file):
+        self.expression.generate(generator, out_file)
+        print("ret", file=out_file)
+
 class PutcNode(AST):
     def __init__(self, token: Token, expression: AST):
         super().__init__(token)
@@ -594,6 +613,7 @@ class PutcNode(AST):
         print("syscall", file=out_file)
 
 
+
 class VariableDeclarationNode(AST):
     def __init__(self, token: Token, variable: Token):
         super().__init__(token)
@@ -603,6 +623,7 @@ class VariableDeclarationNode(AST):
         return dict(variable=self.variable.to_dict())
 
     def generate(self, generator, out_file):
+        out_file = out_file
         generator.variables[self.variable.literal] = None
 
 
@@ -716,6 +737,19 @@ class ConstDeclarationNode(AST):
             exit(1)
         generator.constants[self.identifier.literal] = self.value.literal
 
+class VariableDeclarationAndAssignNode(AST):
+    def __init__(self, token: Token, identifier: Token, assign_node: AST):
+        super().__init__(token)
+        self.assign_node = assign_node
+        self.identifier = identifier
+
+    def to_dict(self):
+        return dict(assign_node=self.assign_node.to_dict())
+
+    def generate(self, generator, out_file):
+        generator.variables[self.identifier.literal] = f"[mem+{generator.memory_depth}]"
+        generator.memory_depth += 8
+        self.assign_node.generate(generator, out_file) 
 
 class Parser:
     def __init__(self, tokens: List[Token]):
@@ -733,11 +767,13 @@ class Parser:
 
     def parse_statement(self):
         if debug:
-            print(self.current_token_location())
+            print("`"+self.tokens[self.cursor].literal+"`", self.current_token_location())
         if self.current_token() == TokenType.IF:
             return self.parse_if()
         if self.current_token() == TokenType.WHILE:
             return self.parse_while()
+        if self.current_token() == TokenType.RETURN:
+            return self.parse_return()
         if self.current_token() == TokenType.OPEN_CURLY_BRACKET:
             return self.parse_block()
         if self.current_token() == TokenType.IDENTIFIER:
@@ -745,8 +781,6 @@ class Parser:
                 return self.parse_table_access_for_assigment()
             if self.tokens[self.cursor + 1].type == TokenType.ASSIGN:
                 return self.parse_assign()
-            if self.tokens[self.cursor + 1].type == TokenType.OPEN_PAREN:
-                return self.parse_function_call()
         if self.current_token() == TokenType.FUNCTION:
             return self.parse_function()
         if self.current_token() == TokenType.PUTC:
@@ -757,7 +791,7 @@ class Parser:
             return self.parse_const_declaration()
 
     def parse_expr(self) -> AST:
-        assert TokenType.TokenType_NUMBERS == 27
+        assert TokenType.TokenType_NUMBERS == 28
         left = self.parse_T()
         if self.current_token() == TokenType.MINUS:
             token = self.chop_token()
@@ -825,7 +859,9 @@ class Parser:
         elif self.current_token() == TokenType.INT:
             return IntNode(self.chop_token())
         elif self.current_token() == TokenType.IDENTIFIER:
-            if self.cursor + 1 < len(self.tokens) and self.tokens[self.cursor + 1].type == TokenType.OPEN_BRACKET:
+            if self.cursor + 1 < len(self.tokens) and self.tokens[self.cursor + 1].type == TokenType.OPEN_PAREN:
+                return self.parse_function_call() 
+            elif self.cursor + 1 < len(self.tokens) and self.tokens[self.cursor + 1].type == TokenType.OPEN_BRACKET:
                 token = self.chop_token()
                 self.chop_token()  # token [
                 index = self.parse_expr()
@@ -958,6 +994,15 @@ class Parser:
             exit(1)
         return PutcNode(token, expression)
 
+    def parse_return(self) -> AST:
+        token = self.chop_token()
+        expression = self.parse_expr()
+        ct = self.chop_token()
+        if ct.type != TokenType.SEMICOLON:
+            print(f"{ct.location}:ERROR: missing `;`")
+            exit(1)
+        return ReturnNode(token, expression)
+
     def parse_function_call(self) -> AST:
         token = self.chop_token()
         self.chop_token()  # token (
@@ -972,31 +1017,29 @@ class Parser:
                     close_paren = True
                 case _:
                     print(f"{self.chop_token().location}: ERROR: no comma after value in function call")
-                    exit(1)
-        self.chop_token()  # I don't know why, and i don't want to think about it
-        ct = self.chop_token()
-        if ct.type != TokenType.SEMICOLON:
-            print(f"{ct.location}: ERROR: missing semicolon")
-            exit(1)
+        self.chop_token() # drop )
         return fun_call
 
     def parse_var_declaration(self) -> AST:
         token = self.chop_token()
-        variable = self.chop_token()
-        if self.current_token() == TokenType.OPEN_BRACKET:
-            self.chop_token()
+        variable = self.tokens[self.cursor]
+        if self.tokens[self.cursor+1].type == TokenType.OPEN_BRACKET:
+            self.chop_token() # chop var
+            self.chop_token() # chop [ 
             length = self.chop_token()
-            self.chop_token()
+            self.chop_token() # chop ]
             semicolon = self.chop_token()
             if semicolon.type != TokenType.SEMICOLON:
                 print(f"{semicolon.location}: ERROR: missing semicolon")
                 exit(1)
             return TableDeclarationNode(token, variable, length)
         else:
-            semicolon = self.chop_token()
-            if semicolon.type != TokenType.SEMICOLON:
-                print(f"{semicolon.location}: ERROR: missing semicolon")
-                exit(1)
+            next_token = self.tokens[self.cursor+1]
+            if next_token.type == TokenType.ASSIGN:
+                parse_eq = self.parse_assign()
+                return VariableDeclarationAndAssignNode(token, variable, parse_eq)
+            self.chop_token()
+            self.chop_token()
             return VariableDeclarationNode(token, variable)
 
     def parse_table_access_for_assigment(self) -> AST:
@@ -1013,6 +1056,7 @@ class Parser:
                 print(f"{ct.location}:ERROR: missing `;`")
                 exit(1)
             return AssignNode(optoken, tan, expression)
+        return tan
 
     def parse_const_declaration(self):
         token = self.chop_token()
